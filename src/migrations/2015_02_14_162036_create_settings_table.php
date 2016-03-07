064e34d22d02954e334f5e429feaf6c2ff92d38e
Tobias Kündig <tobias.kuendig@offlinegmbh.ch>
2016-03-07 10:36:01 +0100

    Fixed bug in migration definition

--- src/migrations/2015_02_14_162036_create_settings_table.php
+++ src/migrations/2015_02_14_162036_create_settings_table.php
@@ -16,7 +16,7 @@ class CreateSettingsTable extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->increments('id');
             $table->string('key')->index()->unique();
-            $table->string('value')->longText();
+            $table->longText('value');
         });
     }
 
